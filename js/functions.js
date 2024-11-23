1157ded5a36e43862dcfd6f436dd13118146fa82
VlasovaNatalya <natatavv@gmail.com>
2024-11-23 16:42:20 +0700

    task 2

---
+++ js/functions.js
@@ -0,0 +1,41 @@
+function stringLenght (string, maxLen){
+  return string.length <= maxLen;
+}
+
+function checkPolindrom (string) {
+  let normalized = string.replaceAll(" ", "").toLowerCase();
+  return normalized === normalized.split("").reverse().join("");
+}
+
+function findNumbers (str) {
+  let string = str.toString();
+  let numbers = '';
+  for (let char of string) {
+    let num = parseInt(char, 10);
+    if (!Number.isNaN(num)) {
+      numbers += num;
+    }
+  }
+  if (numbers === '') {
+    return NaN;
+  }
+  return parseInt(numbers, 10);
+}
+
+// console.log(stringLenght('проверяемая строка', 20));
+// console.log(stringLenght('проверяемая строка', 18));
+// console.log(stringLenght('проверяемая строка', 10));
+
+// console.log(checkPolindrom('топот'));
+// console.log(checkPolindrom('ДовОд'));
+// console.log(checkPolindrom('Кекс'));
+// console.log(checkPolindrom('Лёша на полке клопа нашёл '));
+
+console.log(findNumbers('2023 год'));
+console.log(findNumbers('ECMAScript 2022'));
+console.log(findNumbers('1 кефир, 0.5 батона'));
+console.log(findNumbers('агент 007'));
+console.log(findNumbers('а я томат'));
+console.log(findNumbers(2023));
+console.log(findNumbers(-1));
+console.log(findNumbers(1.5));
