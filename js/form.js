f8ddced76b30a060670a2fbcf5e4446823c0b7e9
VlasovaNatalya <natatavv@gmail.com>
2024-12-21 16:13:07 +0700

    validate comments and hashtags

--- js/form.js
+++ js/form.js
@@ -4,17 +4,48 @@ const postForm = document.querySelector('.img-upload__form');
 const imgUploadInput = document.querySelector('.img-upload__input');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadCancel = document.querySelector('.img-upload__cancel');
-const pristine = new Pristine(postForm);
+const textDescription = document.querySelector('.text__description');
+const textHashtags = document.querySelector('.text__hashtags');
+
+const pristine = new Pristine(postForm, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text',
+});
+
+function validateComment (value) {
+  return value.length <= 5;
+}
+
+pristine.addValidator(
+  textDescription,
+  validateComment,
+  'Комментарий не должен превышать 140 символов.'
+);
+
+function validateHashtags (value) {
+  const hashtags = value.trim().split(' ');
+
+  for (let i = 0; i < hashtags.length; i++) {
+    const tag = hashtags[i].toLowerCase();
+    if (tag && !/^#[^\s#]+$/i.test(tag)) {
+      return false;
+    }
+    if (hashtags.slice(0, i).includes(tag)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+pristine.addValidator(
+  textHashtags,
+  validateHashtags,
+  'Неправильный формат хэштэгов.'
+);
 
 postForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const isValid = pristine.validate();
-  if (isValid) {
-    console.log('Можно отправлять');
-  } else {
-    console.log('Форма невалидна');
-  }
+  pristine.validate();
 });
 
 function openModal () {
@@ -42,5 +73,3 @@ imgUploadCancel.addEventListener('click', () => {
 imgUploadInput.addEventListener('change', () => {
   openModal();
 });
-
-
