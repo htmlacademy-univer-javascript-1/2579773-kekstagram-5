150b9855d815ad3e45519f19a0b15a89902313b4
VlasovaNatalya <natatavv@gmail.com>
2024-12-30 16:21:23 +0700

    fixing mistakes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-import {renderAllPosts} from './rendering.js';
-import {addUploadListeners} from './form.js';
-import {uploadPhoto} from './upload-photo.js';
-import {getData} from './remote-server.js';
-import {sendPhoto} from './form.js';
+import {addUploadListeners, setupPhotoSubmission} from './form.js';
+import {setupPhotoPreview} from './upload-photo.js';
+import {loadPostsFromServer} from './remote-server.js';
 
-
-const loadPosts = getData((data) => {
-  renderAllPosts(data);
-}, console.error);
-
-loadPosts();
-sendPhoto();
-
+loadPostsFromServer();
 addUploadListeners();
-uploadPhoto();
+setupPhotoPreview();
+setupPhotoSubmission();
